601548e23843261a1966e133988ba216cb2fcaa4
chakri68 <chakridevireddy69@gmail.com>
2023-10-02 18:13:14 +0530

    Added configs for eslint, prettier, and husky

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// .eslintrc.js
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended", // Use this to disable ESLint rules that conflict with Prettier
+  ],
+  plugins: ["prettier"],
+  rules: {
+    // You can add your specific ESLint rules here if needed
+  },
+};
